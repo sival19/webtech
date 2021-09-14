c91fb5d02dec21484c6063d4255ff9e53fe2ea4f
sival19 <sival19@student.sdu.dk>
2021-09-14 22:52:55 +0200

    more

--- lec02/JS/triangle.js
+++ lec02/JS/triangle.js
@@ -1,7 +1,11 @@
-let triangle = function(x,y) {
+let triangle = function (x, y) {
 
-    for (let i = y; i.length < x+1; i = i + y) {
+    for (let i = y; i.length < x + 1; i = i + y) {
         console.log(i);
     }
 
 }
+let ele = document.getElementById("validationCustom05");
+let text = document.createTextNode("new track");
+ele.appendChild(text);
+document.body.appendChild(para);
