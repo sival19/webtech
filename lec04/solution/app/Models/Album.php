8d1a4818565350b357f53da7e1f5e010e7006500
Miguel Campusano <mica@mmmi.sdu.dk>
2021-09-29 16:09:26 +0200

    lec04 exercise solution

---
+++ lec04/solution/app/Models/Album.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    use HasFactory;
+    
+    protected $casts = ["tracks" => "json"];
+    
+    public function artist() {
+        return $this->belongsTo(Artist::class);
+    }
+}
---
+++ lec04/solution/resources/views/albums/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Album library</title>
+    <link rel="stylesheet" href="<?php echo asset("css\app.css")?>">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+
+  </head>
+  <body>
+      <h1>Welcome!</h1>
+
+      <div>
+        <h2>Albums</h2>
+        <div id='albums'>
+          <?php if(empty($albums)): ?>
+            <p>No albums found :(</p>
+          <?php else: ?>
+            <ul>
+              <?php foreach($albums as $album): ?>
+                <li>
+                  <?php echo $album->artist->name.": ".$album->name."(".count($album->tracks)." songs)"; ?>
+                </li>
+              <?php endforeach;?>
+            </ul>
+          <?php endif;?>
+        </div>
+        <a href='<?php echo route("albums.create")?>'><button>Add new</button></a>
+      </div>
+
+      
+  </body>
+</html>
