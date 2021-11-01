05597eaf05f40790fb0eca17baac0be8c73d6776
Miguel Campusano <mica@mmmi.sdu.dk>
2021-11-01 16:57:57 +0100

    lec06 solution

---
+++ lec06/solution/storage/framework/views/5a3881875932bf5fc2a37558b889db862031627e.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/mica/Work/Personal/knowledge/courses/teaching/current/web_tech/2021/lectures/lec04/assignment/solution/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
